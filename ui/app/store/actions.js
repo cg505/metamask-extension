c99292ac68d4acef966aee9f3cdabbfa237b6a5a
Dan Miller <danjm.com@gmail.com>
2019-07-18 10:02:56 -0230

    Add toggle in settings for 3box syncing

--- ui/app/store/actions.js
+++ ui/app/store/actions.js
@@ -329,6 +329,7 @@ var actions = {
   setUseNativeCurrencyAsPrimaryCurrencyPreference,
   setShowFiatConversionOnTestnetsPreference,
   setAutoLogoutTimeLimit,
+  setThreeBoxSyncing,
 
   // Onboarding
   setCompletedOnboarding,
@@ -2515,6 +2516,24 @@ function setShowFiatConversionOnTestnetsPreference (value) {
 
 function setAutoLogoutTimeLimit (value) {
   return setPreference('autoLogoutTimeLimit', value)
+}
+
+function setThreeBoxSyncing (newThreeBoxSyncingState) {
+  return dispatch => {
+    dispatch(actions.showLoadingIndication())
+    return new Promise((resolve, reject) => {
+      background.setThreeBoxSyncing(newThreeBoxSyncingState, (err) => {
+        dispatch(actions.hideLoadingIndication())
+
+        if (err) {
+          dispatch(actions.displayWarning(err.message))
+          return reject(err)
+        }
+
+        resolve()
+      })
+    })
+  }
 }
 
 function setCompletedOnboarding () {
